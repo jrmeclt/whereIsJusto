2a6a861dbae20349f7b36055de70904c40250cca
Jerome Clot <jerome.clot@heig-vd.ch>
2014-03-03 13:04:44 +0100

    bouffe

--- js/query.js
+++ js/query.js
@@ -29,12 +29,24 @@ $("document").ready(function(){
    	 	//return $jform.check();
 	});
 	
-	$('.result').live('click', '#headerResult', function() {
+	$('#page').on('click', '.pageSuivante',function() {
+		//event.preventDefault();
+		//var page = $(this).attr('value');
+		var page = $(".pageSuivante").attr('id');
+		var parent = $(".nbpage").attr('id');
+
+		console.log(parent);
+		query('','',page,parent);
+   	 	//return $jform.check();
+	});
+	
+	$('.hidden').on('click', '#headerResult', function() {
 		
-		console.log("on");   	 	//return $jform.check();
+		//console.log("on");   	 	//return $jform.check();
+		//$('.hidden',this).show();
 		 //$(this).children().show();
 		// $('.hidden',this).children('.hidden').toggle();
-		$(this).children('.hidden').show();
+		//$(this).children('.hidden').show();
 		//$('hidden'.this).childNodes.toggle();
 		 //$(this).after("<p>Another paragraph!</p>");
 	});
@@ -47,11 +59,15 @@ $("document").ready(function(){
     $( "#accordion" ).accordion();
   });
 
-function query(id,parent){
+function query(id,parent,page,pageActuelRecu){
 	id = id || "";
 	parent = parent || "";
+	page = page || "";
+	pageActuelRecu = pageActuelRecu || "";
 	//reqVal= $(".query").val();
 	//console.log(reqVal);
+	//console.log(page);
+	//var page = 100;
 	
 	console.log(id.length);
 	
@@ -77,9 +93,17 @@ function query(id,parent){
 	
 	
 	request['rows'] = $(".rows option:selected").val();
-	request['start'] = 0;	
+	
+	if(page.length){
+	console.log("bouhouuou");
+	request['start'] = page;
+	}
+	else{
+	request['start']=0;
+	}
+		
 	request['wt'] = "json";
-	request['']= "";
+	//request['']= "";
 	var dataType = 'jsonp';
 	var url="http://localhost:8983/solr/select";	
 	/*$.ajax(dataType, url, request, function(result, status, data) {	
@@ -160,11 +184,24 @@ function query(id,parent){
 			if(data.response.numFound>request['rows']){
 				var resultat = data.response.numFound;
 				var resultatparpage = request['rows'];
+				console.log(request['start']);
+				var nextStart= Number(request['start'])+Number(resultatparpage);
+				console.log(nextStart);
 				console.log(resultatparpage);
 				var nbpagebrut = resultat / resultatparpage;
 				var nbpage = Math.ceil(nbpagebrut);
+				var pageActuel=Number(pageActuelRecu)+1;
+				if(isNaN(pageActuel)) {
+					var pageActuel = 1;
+				}
 				console.log(nbpage);
-				html+="<p>page1 de "+nbpage+"</p>";
+				
+				$("#page").children().remove();
+				
+				var pagesuivante="<p class='nbpage' id='"+pageActuel+"'>page "+pageActuel+" de "+nbpage+"</p><p class='pageSuivante' id='"+nextStart+"'>Page suivante</p>";
+				//html += ...;
+				$("#page").append(pagesuivante);
+
 			}
 			
 			$("#results").html(html);
@@ -196,9 +233,9 @@ function afficherVille(city){
 }
 
 function afficherMedia(media){
-						//console.log(media);
+						console.log(media);
 	
-	var html = '<div class="result" id='+media.role+'><img src="images/picto/'+media.role+'.gif"/> '+media["title"]+'</div>';
+	var html = '<div class="result" id='+media.role+'><img src="images/picto/'+media.role+'.gif"/> '+media["title"]+'<a href="http://comem.trucmu.ch/mrm/medias/'+media["groupname"]+'/'+media["role"]+'/'+media["filename"]+'">Vers le fichier</a></div>';
 	
 	return html;				
 
